f3a3713daeafbacf3e159280f80e16127d4840db
Anton Heestand <anton.heestand@bontouch.com>
2021-03-21 15:44:45 +0100

    Map Node

--- Sources/Canvas/Canvas.swift
+++ Sources/Canvas/Canvas.swift
@@ -2,9 +2,11 @@ import Foundation
 import SwiftUI
 import CoreGraphicsExtensions
 
-public class Canvas: ObservableObject {
+public class Canvas: ObservableObject, Identifiable {
     
     public weak var delegate: CanvasDelegate?
+    
+    public let id: UUID
     
     let physics: Bool
     let snapGridToAngle: Angle?
@@ -13,8 +15,18 @@ public class Canvas: ObservableObject {
     @Published public var scale: CGFloat = 1.0
     @Published public var angle: Angle = .zero
     public var coordinate: CanvasCoordinate {
-        CanvasCoordinate(offset: offset, scale: scale, angle: angle)
+        get {
+            CanvasCoordinate(offset: offset, scale: scale, angle: angle)
+        }
+        set {
+            offset = newValue.offset
+            scale = newValue.scale
+            angle = newValue.angle
+        }
     }
+    
+    /// Only used for centering.
+    @Published public var size: CGSize = .zero
     
     @Published var interactions: Set<CanvasInteraction> = []
     @Published var panInteraction: CanvasInteraction? = nil
@@ -25,26 +37,40 @@ public class Canvas: ObservableObject {
     @Published var mouseLocation: CGPoint? = nil
 
     public init(physics: Bool = false, snapGridToAngle: Angle? = nil) {
+        self.id = UUID()
         self.physics = physics
         self.snapGridToAngle = snapGridToAngle
     }
+}
+
+// MARK: - Equatable
+
+extension Canvas: Equatable {
     
-    public func reset(animated: Bool = false, offset: CGPoint = .zero) {
+    public static func == (lhs: Canvas, rhs: Canvas) -> Bool {
+        lhs.id == rhs.id
+    }
+}
+
+// MARK: - Reset
+
+extension Canvas {
+    
+    public func reset(animated: Bool = false) {
         let currentOffset: CGPoint = self.offset
         let currentScale: CGFloat = self.scale
         let currentAngle: Angle = self.angle
+        let currentSize: CGSize = self.size
         if animated {
             CanvasAnimation.animate(for: 0.25, ease: .easeInOut) { fraction in
-                self.offset = currentOffset * (1.0 - fraction) + offset * fraction
+                self.offset = currentOffset * (1.0 - fraction) + (currentSize.point / 2) * fraction
                 self.scale = currentScale * (1.0 - fraction) + fraction
                 self.angle = Angle(degrees: currentAngle.degrees * (1.0 - Double(fraction)))
             }
-
         } else {
             self.offset = offset
             self.scale = 1.0
             self.angle = .zero
         }
     }
-    
 }
--- Sources/Canvas/Views/CanvasView.swift
+++ Sources/Canvas/Views/CanvasView.swift
@@ -13,19 +13,19 @@ public struct CanvasView: View {
         ZStack(alignment: .topLeading) {
             
             // Touches
-//            #if DEBUG
-//            ForEach(canvasInteractions) { interaction in
-//                Circle()
-//                    .foregroundColor((interaction == canvasPanInteraction) ? .blue :
-//                                        (interaction == canvasPinchInteraction?.0 ||
-//                                            interaction == canvasPinchInteraction?.1) ? .purple :
-//                                        canvasDragInteractions.contains(where: { $0.value == interaction }) ? .orange : .primary)
-//                    .opacity(interaction.active ? 1.0 : 0.25)
-//                    .frame(width: 50, height: 50)
-//                    .offset(x: interaction.location.x - 25,
-//                            y: interaction.location.y - 25)
-//            }
-//            #endif
+            #if DEBUG
+            ForEach(Array(canvas.interactions)) { interaction in
+                Circle()
+                    .foregroundColor((interaction == canvas.panInteraction) ? .blue :
+                                        (interaction == canvas.pinchInteraction?.0 ||
+                                            interaction == canvas.pinchInteraction?.1) ? .purple :
+                                        canvas.dragInteractions.contains(where: { $0.interaction == interaction }) ? .orange : .primary)
+                    .opacity(interaction.active ? 1.0 : 0.25)
+                    .frame(width: 50, height: 50)
+                    .offset(x: interaction.location.x - 25,
+                            y: interaction.location.y - 25)
+            }
+            #endif
             
             // Interact
             CanvasInteractViewRepresentable(canvas: canvas)
