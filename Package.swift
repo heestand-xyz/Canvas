400c2aa2cf6784bca12bf7d2883dd32d8677a723
Anton Heestand <anton.heestand@gmail.com>
2021-12-08 10:05:14 +0100

    Tags

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let package = Package(
             targets: ["Canvas"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/heestand-xyz/MultiViews", from: "1.3.1"),
-        .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "1.0.1"),
-        .package(url: "https://github.com/heestand-xyz/DisplayLink", from: "1.0.1"),
-        .package(url: "https://github.com/heestand-xyz/Logger", from: "0.1.0"),
+        .package(url: "https://github.com/heestand-xyz/MultiViews", from: "1.5.0"),
+        .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "1.2.1"),
+        .package(url: "https://github.com/heestand-xyz/DisplayLink", from: "1.0.2"),
+        .package(url: "https://github.com/heestand-xyz/Logger", from: "0.2.1"),
     ],
     targets: [
         .target(
